28db9a601c5c604b40f231325ea14e6f3dbd8992
IrinaMank <zapirinamank@gmail.com>
2017-06-29 10:15:03 +0700

    Initial commit

---
+++ app/src/main/java/com/example/irina/wtw/MoviesApiService.java
@@ -0,0 +1,12 @@
+package com.example.irina.wtw;
+
+/**
+ * Created by Irina on 28.06.2017.
+ */
+
+public interface MoviesApiService {
+    @GET("/search/movie")
+    void searchMovie(){
+
+    }
+}
