ae4577f23bd4df12212e54705150db0ee2fe4ed3
IrinaMank <zapirinamank@gmail.com>
2018-05-05 14:12:01 +0700

    Profile activity with signOut

--- app/src/main/java/com/example/irina/wtw/activities/AuthActivity.java
+++ app/src/main/java/com/example/irina/wtw/activities/AuthActivity.java
@@ -1,6 +1,8 @@
 package com.example.irina.wtw.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -10,11 +12,15 @@ import android.util.Log;
 import android.view.View;
 
 import com.example.irina.wtw.R;
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthCredential;
@@ -27,12 +33,14 @@ public class AuthActivity extends AppCompatActivity {
 
     private static final String TAG = "GoogleActivity";
     private static final int RC_SIGN_IN = 9001;
+    private static SharedPreferences.Editor editor;
 
     // [START declare_auth]
     private FirebaseAuth mAuth;
     // [END declare_auth]
 
     private GoogleSignInClient mGoogleSignInClient;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +53,8 @@ public class AuthActivity extends AppCompatActivity {
                 .build();
         mAuth = FirebaseAuth.getInstance();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        SharedPreferences sharedPref = this.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
     }
 
     @Override
@@ -57,6 +67,20 @@ public class AuthActivity extends AppCompatActivity {
     public void signIn(View v) {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    public void signOut(View v) {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        updateUI(null);
+                    }
+                });
     }
 
     @Override
@@ -85,6 +109,10 @@ public class AuthActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             Log.d(TAG, "signInWithCredential:success");
                             FirebaseUser user = mAuth.getCurrentUser();
+                            editor.putString(getString(R.string.saved_user_name), user.getDisplayName());
+                            editor.putString(getString(R.string.saved_user_email), user.getEmail());
+                            editor.putString(getString(R.string.saved_user_photo), user.getPhotoUrl().toString());
+                            editor.commit();
                             updateUI(user);
                         } else {
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
--- app/src/main/java/com/example/irina/wtw/activities/MainActivity.java
+++ app/src/main/java/com/example/irina/wtw/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.irina.wtw.activities;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -10,9 +11,11 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.irina.wtw.db.WantsTable;
 import com.example.irina.wtw.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     WantsTable dbAdapter;
@@ -70,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                         fragmentClass = Search.class;
                         break;
                     case R.id.nav_profile:
-                        fragmentClass = AuthActivity.class;
+                        fragmentClass = ProfileActivity.class;
                         break;
                 }
                 try {
@@ -89,6 +92,13 @@ public class MainActivity extends AppCompatActivity {
             }
         };
         return listener;
+    }
+
+    public void signOut(View v) {
+
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, AuthActivity.class);
+        startActivity(intent);
     }
 }
 
